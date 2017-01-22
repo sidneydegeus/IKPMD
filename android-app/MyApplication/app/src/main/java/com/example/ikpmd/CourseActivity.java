02ffb04e0224ba1d9c8ad97ce8c98891bfff3807
DESKTOP-EDADN30\Jurre <jurre2005@hotmail.com>
2017-01-22 12:55:18 +0000

    set up android app with standard navigation

---
+++ android-app/MyApplication/app/src/main/java/com/example/ikpmd/CourseActivity.java
@@ -0,0 +1,20 @@
+package com.example.ikpmd;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class CourseActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_course, null, false);
+        mDrawer.addView(contentView, 0);
+        setActivityTitle("Vakken");
+    }
+}
---
+++ android-app/MyApplication/app/src/main/java/com/example/ikpmd/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.ikpmd;
+
+import android.content.Intent;
+import android.support.v4.view.WindowCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    protected DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(WindowCompat.FEATURE_ACTION_BAR);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(toolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, R.string.open, R.string.close);
+
+        mDrawer.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goToOverview(MenuItem item) {
+        startActivity(new Intent(MainActivity.this, OverviewActivity.class));
+    }
+
+    public void goToCourses(MenuItem menuItem) {
+        startActivity(new Intent(MainActivity.this, CourseActivity.class));
+    }
+
+    public void goToSettings(MenuItem menuItem) {
+        startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+    }
+
+    protected void setActivityTitle(String title) {
+        TextView textView = (TextView) findViewById(R.id.screenTitle);
+        textView.setText(title);
+    }
+}
